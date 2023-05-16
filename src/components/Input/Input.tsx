79b22479404702912eb54130310bdc88281079b1
Jose IV <ivjosesantos@gmail.com>
2023-05-16 17:46:23 +0800

    Input initial placeholder

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,27 @@
+ 
+// 2. Input Field Component: Implement an Input Field component that includes
+// different states (default, focus, error) and types (text, email, password, etc.).
+
+// Design Principles:
+// Component Design: Each component should be self-contained and reusable. It
+// should accept props to modify its behavior or style.
+
+// Scalability: The library should be designed in a way that new components can be
+// easily added in the future.
+
+// Testing: Write unit tests for your components using a testing library of your choice
+// (Jest, React Testing Library, etc.). Ensure all major functionality is covered.
+
+// Responsive Design: The components should be responsive and work well on
+// different screen sizes.
+
+// Accessibility: The components are usable via keyboard navigation and contain
+// appropriate properties.
+
+function Input() {
+  return (
+    <div>Input</div>
+  )
+}
+
+export default Input
