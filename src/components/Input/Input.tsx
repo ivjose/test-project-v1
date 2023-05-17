b7c9d7b53332b7364e3a22116546631a1494c77e
Jose IV <ivjosesantos@gmail.com>
2023-05-17 11:49:38 +0800

    Improve input code

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -10,32 +10,15 @@ export interface InputProps
   error?: string;
 }
 
-function Input({ type, label, error, ...rest }: InputProps) {
+function Input({ label, error, ...rest }: InputProps) {
   const inputClassName = classNames('input-field', {
     'input-field--error': error,
   });
 
-  if (type === 'textarea') {
-    return (
-      <div className="input-container">
-        <label htmlFor={rest.id} className="input-label">
-          {label}
-        </label>
+  let InputElement = 'input';
 
-        <textarea
-          className={inputClassName}
-          aria-invalid={error ? 'true' : 'false'}
-          aria-required={rest.required ? 'true' : 'false'}
-          aria-describedby={error ? `${rest.id}-error` : ''}
-          {...rest}
-        />
-        {error && (
-          <div id={`${rest.id}-error`} role="alert" className="error-message">
-            {error}
-          </div>
-        )}
-      </div>
-    );
+  if (rest.type === 'textarea') {
+    InputElement = 'textarea';
   }
 
   return (
@@ -44,8 +27,7 @@ function Input({ type, label, error, ...rest }: InputProps) {
         {label}
       </label>
 
-      <input
-        type={type}
+      <InputElement
         className={inputClassName}
         aria-invalid={error ? 'true' : 'false'}
         aria-required={rest.required ? 'true' : 'false'}
